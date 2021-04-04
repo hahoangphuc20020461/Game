90f4b247a2285e710c6d9ef50d73901ea4527f28
hahoangphuc <80321171+hahoangphuc20020461@users.noreply.github.com>
2021-04-04 16:23:40 +0700

    Update LTexture.h

--- LTexture.h
+++ LTexture.h
@@ -4,44 +4,37 @@
 class LTexture
 {
 	public:
-		//Initializes variables
+	
 		LTexture();
 
-		//Deallocates memory
+		
 		~LTexture();
 
-		//Loads image at specified path
+	
 		bool loadFromFile( std::string path );
 
 		#if defined(SDL_TTF_MAJOR_VERSION)
-		//Creates image from font string
+		
 		bool loadFromRenderedText( std::string textureText, SDL_Color textColor );
 		#endif
 
-		//Deallocates texture
 		void free();
 
-		//Set color modulation
 		void setColor( Uint8 red, Uint8 green, Uint8 blue );
 
-		//Set blending
 		void setBlendMode( SDL_BlendMode blending );
 
-		//Set alpha modulation
 		void setAlpha( Uint8 alpha );
 
-		//Renders texture at given point
 		void render( int x, int y , SDL_Rect* clip = NULL, double angle = 0.0, SDL_Point* center = NULL, SDL_RendererFlip flip = SDL_FLIP_NONE);
-
-		//Gets image dimensions
+s
 		int getWidth();
 		int getHeight();
 
 	private:
-		//The actual hardware texture
+
 		SDL_Texture* mTexture;
 
-		//Image dimensions
 		int mWidth;
 		int mHeight;
 };
@@ -55,7 +48,6 @@ LTexture gStartPromptTexture;
 
 LTexture::LTexture()
 {
-	//Initialize
 	mTexture = NULL;
 	mWidth = 0;
 	mHeight = 0;
@@ -63,19 +55,15 @@ LTexture::LTexture()
 
 LTexture::~LTexture()
 {
-	//Deallocate
 	free();
 }
 
 bool LTexture::loadFromFile( std::string path )
 {
-	//Get rid of preexisting texture
 	free();
 
-	//The final texture
 	SDL_Texture* newTexture = NULL;
 
-	//Load image at specified path
 	SDL_Surface* loadedSurface = IMG_Load( path.c_str() );
 	if( loadedSurface == NULL )
 	{
@@ -83,10 +71,8 @@ bool LTexture::loadFromFile( std::string path )
 	}
 	else
 	{
-		//Color key image
 		SDL_SetColorKey( loadedSurface, SDL_TRUE, SDL_MapRGB( loadedSurface->format, 0, 0xFF, 0xFF ) );
 
-		//Create texture from surface pixels
         newTexture = SDL_CreateTextureFromSurface( gRenderer, loadedSurface );
 		if( newTexture == NULL )
 		{
@@ -94,7 +80,6 @@ bool LTexture::loadFromFile( std::string path )
 		}
 		else
 		{
-			//Get image dimensions
 			mWidth = loadedSurface->w;
 			mHeight = loadedSurface->h;
 		}
@@ -103,14 +88,12 @@ bool LTexture::loadFromFile( std::string path )
 		SDL_FreeSurface( loadedSurface );
 	}
 
-	//Return success
 	mTexture = newTexture;
 	return mTexture != NULL;
 }
 
 void LTexture::free()
 {
-	//Free texture if it exists
 	if( mTexture != NULL )
 	{
 		SDL_DestroyTexture( mTexture );
@@ -122,9 +105,7 @@ void LTexture::free()
 
 void LTexture::render( int x, int y, SDL_Rect* clip, double angle, SDL_Point* center, SDL_RendererFlip flip  )
 {
-	//Set rendering space and render to screen
 	SDL_Rect renderQuad = { x, y, mWidth, mHeight };
-	//Set clip rendering dimensions
 	if( clip != NULL )
 	{
 		renderQuad.w = clip->w;
@@ -148,33 +129,27 @@ int LTexture::getHeight()
 
 void LTexture::setColor( Uint8 red, Uint8 green, Uint8 blue )
 {
-	//Modulate texture rgb
 	SDL_SetTextureColorMod( mTexture, red, green, blue );
 }
 
 void LTexture::setBlendMode( SDL_BlendMode blending )
 {
-	//Set blending function
 	SDL_SetTextureBlendMode( mTexture, blending );
 }
 
 void LTexture::setAlpha( Uint8 alpha )
 {
-	//Modulate texture alpha
 	SDL_SetTextureAlphaMod( mTexture, alpha );
 }
 
 #if defined(SDL_TTF_MAJOR_VERSION)
 bool LTexture::loadFromRenderedText( std::string textureText, SDL_Color textColor )
 {
-	//Get rid of preexisting texture
 	free();
 
-	//Render text surface
 	SDL_Surface* textSurface = TTF_RenderText_Solid( gFont, textureText.c_str(), textColor );
 	if( textSurface != NULL )
 	{
-		//Create texture from surface pixels
         mTexture = SDL_CreateTextureFromSurface( gRenderer, textSurface );
 		if( mTexture == NULL )
 		{
@@ -182,12 +157,10 @@ bool LTexture::loadFromRenderedText( std::string textureText, SDL_Color textColo
 		}
 		else
 		{
-			//Get image dimensions
 			mWidth = textSurface->w;
 			mHeight = textSurface->h;
 		}
 
-		//Get rid of old surface
 		SDL_FreeSurface( textSurface );
 	}
 	else
@@ -195,8 +168,6 @@ bool LTexture::loadFromRenderedText( std::string textureText, SDL_Color textColo
 		printf( "Unable to render text surface! SDL_ttf Error: %s\n", TTF_GetError() );
 	}
 
-
-	//Return success
 	return mTexture != NULL;
 }
 #endif
