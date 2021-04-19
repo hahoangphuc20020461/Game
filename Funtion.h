3c7bef3f89f5347de0cf767c32ee5fac70aa54c8
hahoangphuc <80321171+hahoangphuc20020461@users.noreply.github.com>
2021-04-19 21:08:38 +0700

    Update Funtion.h

--- Funtion.h
+++ Funtion.h
@@ -17,10 +17,8 @@ using namespace std;
 
 #define MAX_HEIGHT 300
 
-//#define ENEMY_MAX_HEIGHT 320
-//#define ENEMY_MIN_HEIGHT 350
 
-#define ENEMY_POSITION_RANGE 250
+#define ENEMY_POSITION_RANGE 300
 
 //#define BASE_OFFSET_SPEED 0
 
@@ -28,15 +26,20 @@ using namespace std;
 #define ON_GROUND_ENEMY 0
 
 #define GROUND_SPEED 0
-#define ENEMY_SPEED 5
+#define ENEMY_SPEED 1
 
 #define MAX_ENEMY_WIDTH 100
 
 #define TIME_INCREASEMENT 1
 #define TIME_MAX 1000
+#define SPEED_INCREASEMENT 1
 
 #define SLOW_FRAME_CHAR 4
 #define SLOW_FRAME_ENEMY 4
+
+#define SMALL_BUTTON 1
+#define COMMON_BUTTON 2
+
 
 
 const int SCREEN_WIDTH = 1200;
@@ -46,9 +49,36 @@ static const int GROUND = 630;
 const double BACKGROUND_SPEED = -5;
 const int FLYING_FRAME = 4;
 const int ENEMY_MAX_HEIGHT = 320;
-const int ENEMY_MIN_HEIGHT = 480;
+const int ENEMY_MIN_HEIGHT = 500;
 
+const int COMMON_BUTTON_WIDTH = 150;
+const int COMMON_BUTTON_HEIGHT = 98;
+const int SMALL_BUTTON_WIDTH = 22;
+const int SMALL_BUTTON_HEIGHT = 34;
 
+const int PLAY_BUTON_POSX = 389;
+const int PLAY_BUTTON_POSY= 186;
+const int HELP_BUTTON_POSX = 389;
+const int HELP_BUTTON_POSY = 293;
+const int EXIT_BUTTON_POSX = 389;
+const int EXIT_BUTTON_POSY = 402;
+const int BACK_BUTTON_POSX = 31;
+const int BACK_BUTTON_POSY = 29;
+
+enum ButtonSprite
+{
+    BUTTON_MOUSE_OUT = 0,
+	BUTTON_MOUSE_OVER = 1,
+	BUTTON_TOTAL = 2
+};
+/*
+Button PlayButton(PLAY_BUTON_POSX, PLAY_BUTTON_POSY);
+Button HelpButton(HELP_BUTTON_POSX, HELP_BUTTON_POSY);
+Button ExitButton(EXIT_BUTTON_POSX, EXIT_BUTTON_POSY);
+Button BackButton(BACK_BUTTON_POSX, BACK_BUTTON_POSY);
+Button PauseButton(PAUSE_BUTTON_POSX, PAUSE_BUTTON_POSY);
+Button ContinueButton(CONTINUE_BUTTON_POSX, CONTINUE_BUTTON_POSY);
+*/
 bool init();
 
 //tải media
@@ -59,14 +89,16 @@ void close();
 
 
 SDL_Window* gWindow = NULL;
-
-
 SDL_Renderer* gRenderer = NULL;
 
 TTF_Font* gFont = NULL;
 
+SDL_Rect* gSpriteClips[BUTTON_TOTAL];
 
-
+Mix_Chunk* gJump = NULL;
+Mix_Chunk* gDie = NULL;
+Mix_Chunk* gMenu = NULL;
+Mix_Chunk* gClick = NULL;
 
 
 #endif
