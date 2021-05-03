46e9aa3c275f6106cc0bb6cf93d073a74908414b
hahoangphuc <80321171+hahoangphuc20020461@users.noreply.github.com>
2021-05-03 21:55:52 +0700

    Update LTexture.h

--- LTexture.h
+++ LTexture.h
@@ -4,13 +4,12 @@
 class LTexture
 {
 	public:
-		//Initializes variables
+
 		LTexture();
 
-		//Deallocates memory
 		~LTexture();
 
-		//Loads image at specified path
+		//hàm load ảnh
 		bool loadFromFile( std::string path );
 
 		#if defined(SDL_TTF_MAJOR_VERSION)
@@ -18,30 +17,22 @@ class LTexture
 		bool loadFromRenderedText( std::string textureText, SDL_Color textColor );
 		#endif
 
-		//Deallocates texture
 		void free();
 
-		//Set color modulation
 		void setColor( Uint8 red, Uint8 green, Uint8 blue );
 
-		//Set blending
 		void setBlendMode( SDL_BlendMode blending );
 
-		//Set alpha modulation
 		void setAlpha( Uint8 alpha );
 
-		//Renders texture at given point
-		void render( int x, int y , SDL_Rect* clip = NULL, double angle = 0.0, SDL_Point* center = NULL, SDL_RendererFlip flip = SDL_FLIP_NONE);
+		void render( int x, int y , SDL_Rect* clip = NULL,double angle = 0.0, SDL_Point* center = NULL, SDL_RendererFlip flip = SDL_FLIP_NONE);
 
-		//Gets image dimensions
 		int getWidth();
 		int getHeight();
 
 	private:
-		//The actual hardware texture
 		SDL_Texture* mTexture;
 
-		//Image dimensions
 		int mWidth;
 		int mHeight;
 };
@@ -50,17 +41,14 @@ LTexture gBackgroundTexture;
 LTexture gCactusTexture;
 LTexture gUfoTexture;
 LTexture gTimeTextTexture;
-LTexture gButtonTexture;
 LTexture gPlayButtonTexture;
 LTexture gInstructionTexture;
-LTexture gBackButtonTexture;
 LTexture gMenuTexture;
-LTexture gHelpButtonTexture;
-LTexture gExitButtonTexture;
+LTexture gLoseTexture;
+
 
 LTexture::LTexture()
 {
-	//Initialize
 	mTexture = NULL;
 	mWidth = 0;
 	mHeight = 0;
@@ -68,19 +56,15 @@ LTexture::LTexture()
 
 LTexture::~LTexture()
 {
-	//Deallocate
 	free();
 }
 
 bool LTexture::loadFromFile( std::string path )
 {
-	//Get rid of preexisting texture
 	free();
 
-	//The final texture
 	SDL_Texture* newTexture = NULL;
 
-	//Load image at specified path
 	SDL_Surface* loadedSurface = IMG_Load( path.c_str() );
 	if( loadedSurface == NULL )
 	{
@@ -88,10 +72,8 @@ bool LTexture::loadFromFile( std::string path )
 	}
 	else
 	{
-		//Color key image
 		SDL_SetColorKey( loadedSurface, SDL_TRUE, SDL_MapRGB( loadedSurface->format, 0, 0xFF, 0xFF ) );
 
-		//Create texture from surface pixels
         newTexture = SDL_CreateTextureFromSurface( gRenderer, loadedSurface );
 		if( newTexture == NULL )
 		{
@@ -99,23 +81,19 @@ bool LTexture::loadFromFile( std::string path )
 		}
 		else
 		{
-			//Get image dimensions
 			mWidth = loadedSurface->w;
 			mHeight = loadedSurface->h;
 		}
 
-		//Get rid of old loaded surface
 		SDL_FreeSurface( loadedSurface );
 	}
 
-	//Return success
 	mTexture = newTexture;
 	return mTexture != NULL;
 }
 
 void LTexture::free()
 {
-	//Free texture if it exists
 	if( mTexture != NULL )
 	{
 		SDL_DestroyTexture( mTexture );
@@ -125,18 +103,16 @@ void LTexture::free()
 	}
 }
 
-void LTexture::render( int x, int y, SDL_Rect* clip, double angle, SDL_Point* center, SDL_RendererFlip flip  )
+void LTexture::render( int x, int y, SDL_Rect* clip , double angle, SDL_Point* center, SDL_RendererFlip flip  )
 {
-	//Set rendering space and render to screen
 	SDL_Rect renderQuad = { x, y, mWidth, mHeight };
-	//Set clip rendering dimensions
 	if( clip != NULL )
 	{
 		renderQuad.w = clip->w;
 		renderQuad.h = clip->h;
 	}
 
-	SDL_RenderCopyEx( gRenderer, mTexture, clip, &renderQuad , angle, center, flip);
+	SDL_RenderCopyEx( gRenderer, mTexture, clip, &renderQuad, angle, center, flip);
 
 }
 
@@ -153,19 +129,16 @@ int LTexture::getHeight()
 
 void LTexture::setColor( Uint8 red, Uint8 green, Uint8 blue )
 {
-	//Modulate texture rgb
 	SDL_SetTextureColorMod( mTexture, red, green, blue );
 }
 
 void LTexture::setBlendMode( SDL_BlendMode blending )
 {
-	//Set blending function
 	SDL_SetTextureBlendMode( mTexture, blending );
 }
 
 void LTexture::setAlpha( Uint8 alpha )
 {
-	//Modulate texture alpha
 	SDL_SetTextureAlphaMod( mTexture, alpha );
 }
 
