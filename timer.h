b65b4c43282f990f7c5b776a0210573447ff8eb1
hahoangphuc <80321171+hahoangphuc20020461@users.noreply.github.com>
2021-05-03 22:00:34 +0700

    Update timer.h

--- timer.h
+++ timer.h
@@ -10,30 +10,22 @@ class LTimer
 		LTimer();
 
 		void start();
-		//void stop();
-		void pause();
-	//	void unpause();
 
 		Uint32 getTicks();
 
 		bool isStarted();
-		bool isPaused();
+
 
     private:
 		Uint32 mStartTicks;
 
-		Uint32 mPausedTicks;
-
-		bool mPaused;
 		bool mStarted;
 };
 
 LTimer::LTimer()
 {
     mStartTicks = 0;
-    mPausedTicks = 0;
 
-    mPaused = false;
     mStarted = false;
 }
 
@@ -41,55 +33,17 @@ void LTimer::start()
 {
     mStarted = true;
 
-    mPaused = false;
-
     mStartTicks = SDL_GetTicks();
-	mPausedTicks = 0;
+
 }
 
-void LTimer::pause()
-{
-    if( mStarted && !mPaused )
-    {
-        mPaused = true;
-
-        mPausedTicks = SDL_GetTicks() - mStartTicks;
-		mStartTicks = 0;
-    }
-}
-/*
-void LTimer::stop()
-{
-    mStarted = false;
-
-    mPaused = false;
-
-	mStartTicks = 0;
-	mPausedTicks = 0;
-}
-
-void LTimer::unpause()
-{
-    if( mStarted && mPaused )
-    {
-
-        mPaused = false;
-
-
-        mStartTicks = SDL_GetTicks() - mPausedTicks;
-
-
-        mPausedTicks = 0;
-    }
-}
-*/
 Uint32 LTimer::getTicks()
 {
 	Uint32 time = 0;
 
     if( mStarted )
     {
-            time = SDL_GetTicks(); //- mStartTicks;
+        time = SDL_GetTicks();
     }
 
     return time;
@@ -100,9 +54,6 @@ bool LTimer::isStarted()
     return mStarted;
 }
 
-bool LTimer::isPaused()
-{
-    return mPaused && mStarted;
-}
+
 
 #endif // TIMER_H_
